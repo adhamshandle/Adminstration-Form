18e57fda2c56e1975aab6ef4a3a3a94d6ab9dc4f
Adham Khaled <adhamkhaled2427@gmail.com>
2018-08-24 10:51:34 +0200

    Add files via upload

---
+++ Assets/JS/Prof.js
@@ -0,0 +1,19 @@
+
+$(".fa-plus").click(function(){
+//$("input[type='text']").toggle();
+$("table").add("<tr class='data'><td><span><i class='fa fa-trash'></i></span><input type='text'></td><td><input type='text'></td><td><input type='text'></td></tr>").appendTo(".tb");
+		
+
+	
+//$(this).toggleClass('fa fa-minus fa fa-plus');
+
+});
+
+$("table").on("click","span",function(){
+	$(this).closest('tr').remove();
+
+});
+
+$("#save").click(function(){
+	//here where you should put the code that will send the data to database
+});
